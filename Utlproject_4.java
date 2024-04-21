0352a563c476f020b01ffccf8af588fc5a58f5e0
s-barouski <s.barouski@mail.ru>
2024-04-21 22:42:55 +0300

    Utlproject_4 next editing class

--- Utlproject_4.java
+++ Utlproject_4.java
@@ -1,3 +1,28 @@
 public class Utlproject_4 {
+    public static void main(String[] args) {
+        float[] floatArray = {34.456f, 5.456f, 12.456f, 48.456f};
+        float min = floatArray[0];
+        for (int i = 1; i < floatArray.length; i++) {
+            if (floatArray[i] < min) {
+                min = floatArray[i];
+            }
+        }
+        System.out.println(min);
 
+        float max = floatArray[0];
+        for (int i = 1; i < floatArray.length; i++) {
+            if (floatArray[i] > max) {
+                max = floatArray[i];
+            }
+        }
+        System.out.println(max);
+
+        float sum = 0;
+        for (int i = 0; i < floatArray.length; i++) {
+            if (floatArray[i] > 0) {
+                sum = sum + floatArray[i];
+            }
+        }
+        System.out.println(sum);
+    }
 }
